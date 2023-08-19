6d18e566ca3e7b2c10998a02d23679bb436469f3
wang-yayu <142365098+wang-yayu@users.noreply.github.com>
2023-08-19 10:17:23 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from tkinter import *
+import os
+import re
 win=Tk()
 win.title("计算器")
+filename = 'record.txt'
 
 def num(a):
   val=result.get()
@@ -22,21 +25,45 @@ def calculate():
     res = eval(expression)
     result.delete(0, END)
     result.insert(0, str(res))
+    lines = []
+    lines.append(expression+"="+str(res))
+    save(lines)
   except:
     result.delete(0, END)
     result.insert(0, "Error")
 
+def save(lst):
+    try:
+      stu_write = open(filename, 'a', encoding='UTF-8')
+    except:
+      stu_write = open(filename, 'w', encoding='UTF-8')
+    for i in lst:
+      stu_write.write(str(i) + '\n')
+    stu_write.close()
+
+def show():
+  if os.path.exists(filename):
+    with open(filename, 'r', encoding='UTF-8') as r_file:
+      stu = r_file.readlines()  # 打开文件并逐行读取内容
+      record.insert(INSERT,stu)
+
+
 def char(a):
   val = result.get()
   result.delete(0, END)
-  result.insert(0, val  + a)
+  result.insert(0, val + a)
 
 
-win.geometry("300x500")
+win.geometry("600x500")
 win.configure(bg="white")
-win.maxsize(300,500)
+win.maxsize(600,500)
+
 result=Entry(win,width=300,relief="solid",justify="center",font="Times 18 bold")
 result.place(width=300,height=125,x=0,y=0)
+record=Text(win,relief="solid",font="Times 8 bold")
+record.place(width=300,height=470,x=300,y=30)
+
+butrecord=Button(win,text="查看历史记录",font="Times 12 bold",relief="groove",command=show).place(width=300,height=30,x=300,y=0)
 left=Button(win,text="(",font="Times 18 bold",relief="groove",command=lambda: char("(")).place(width=75,height=75,x=0,y=125)
 right=Button(win,text=")",font="Times 18 bold",relief="groove",command=lambda: char(")")).place(width=75,height=75,x=75,y=125)
 
